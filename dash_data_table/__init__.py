d452d18c9bd43698d2b1fa0f12cd5adb43c7671c
Jakob Jul Elben <elbenjakobjul@gmail.com>
2021-02-25 13:44:07 +0100

    Fix cdn link and upgrade version

--- dash_data_table/__init__.py
+++ dash_data_table/__init__.py
@@ -4,19 +4,19 @@ import sys
 from dash_data_table import components
 from dash_data_table.components import *
 
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 _current_path = os.path.dirname(os.path.abspath(__file__))
 METADATA_PATH = os.path.join(_current_path, "components", "metadata.json")
 
 _js_dist = [
     {
         "relative_package_path": "components/dash_data_table.min.js",
-        "external_url": f"https://unpkg.com/dash-data-table@{__version__}/dash_data_table/dash_data_table.min.js",
+        "external_url": f"https://unpkg.com/dash-data-table@{__version__}/dash_data_table/components/dash_data_table.min.js",
         "namespace": "dash_data_table",
     },
     {
         "relative_package_path": "components/dash_data_table.min.js.map",
-        "external_url": f"https://unpkg.com/dash-data-table@{__version__}/dash_data_table/dash_data_table.min.js.map",
+        "external_url": f"https://unpkg.com/dash-data-table@{__version__}/dash_data_table/components/dash_data_table.min.js.map",
         "namespace": "dash_data_table",
         "dynamic": True,
     },
